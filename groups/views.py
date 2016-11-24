8d70f24b605fdb0c9bc0f1feafcd7435265344fd
rich-hart <richhohart@gmail.com>
2016-11-24 16:49:27 -0600

    add groups and users

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group
+from rest_framework import viewsets
+from .serializers import GroupSerializer
 
-# Create your views here.
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
