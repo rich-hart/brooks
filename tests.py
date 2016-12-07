95b8b77248908a0bda5cb0c570a263f173acafb1
rich-hart <richhohart@gmail.com>
2016-12-07 13:12:48 -0600

    update profile image

--- tests.py
+++ tests.py
@@ -5,25 +5,53 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from urllib.parse import urljoin
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+
 class TestBasePage(StaticLiveServerTestCase):
+    fixtures = ['fixture.yaml']
     def setUp(self):
-        url = urljoin(self.live_server_url,reverse('home'))
+        url = urljoin(self.live_server_url,reverse('profile'))
         self.driver = webdriver.Chrome()
         self.driver.get(url)
 
-#    def test_nav(self):
-#        import ipdb; ipdb.set_trace()
-#        self.driver.set_window_size(800,600)
-#        topnav_elem = self.driver.find_element_by_id('topnav')
-#        self.assertEqual(topnav_elem.text,'Home\nContact\nAbout')
-#        self.driver.set_window_size(679,600)
-#        self.assertEqual(topnav_elem.text,'Home\n☰')
-#        topnav_elem_list = self.driver.find_element_by_id('topnav_list') 
-#        topnav_elem_list.click()
-#        self.assertEqual(topnav_elem.text,'Home\nContact\nAbout\n☰')
+    def test_nav(self):
+        self.driver.set_window_size(800,600)
+        topnav_elem = self.driver.find_element_by_id('topnav')
+        self.assertEqual(topnav_elem.text,'Profile\nShows\nDonate')
+
+    def tearDown(self):
+        self.driver.close()
+
+
+class TestProfilePage(StaticLiveServerTestCase):
+    fixtures = ['fixture.yaml']
+    def setUp(self):
+        url = urljoin(self.live_server_url,reverse('profile'))
+        self.driver = webdriver.Chrome()
+        self.driver.get(url)
+
+    def test_nav(self):
+        self.driver.set_window_size(800,600)
+        topnav_elem = self.driver.find_element_by_id('topnav')
+        self.assertEqual(topnav_elem.text,'Profile\nShows\nDonate')
 
 
     def tearDown(self):
-#        import time; time.sleep(4)
+        self.driver.close()
+
+class TestShowsPage(StaticLiveServerTestCase):
+    fixtures = ['fixture.yaml']
+    def setUp(self):
+        url = urljoin(self.live_server_url,reverse('shows'))
+        self.driver = webdriver.Chrome()
+        self.driver.get(url)
+
+    def test_nav(self):
+        self.driver.set_window_size(800,600)
+        topnav_elem = self.driver.find_element_by_id('topnav')
+        self.assertEqual(topnav_elem.text,'Profile\nShows\nDonate')
+
+
+    def tearDown(self):
         self.driver.close()
 
