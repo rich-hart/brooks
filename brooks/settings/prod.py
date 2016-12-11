cfdbb6bd16b13a2ebc076a97f1d687d35d56c03c
rich-hart <richhohart@gmail.com>
2016-12-11 12:54:28 -0600

    update static settings

--- brooks/settings/prod.py
+++ brooks/settings/prod.py
@@ -1,16 +1,14 @@
 import os
 from .base import *
 
+DEBUG = False
+
 ALLOWED_HOSTS = [
     '0.0.0.0',
     'localhost',
     'michaelbrooks.herokuapp.com',
     'www.kardashevweb.com',
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR,'static') 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 try:
     from .local import *
